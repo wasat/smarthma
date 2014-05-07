ff4fcf28c6f48c11ccc6b46975e2181502ce8a52
wasat.pl@gmail.com <wasat.pl@gmail.com@1ddc95d8-fd17-99f2-4e67-9d68e992d05e>
2014-05-07 22:27:04 +0000

    Added carousel view and search box to action bar
    git-svn-id: https://smarthma.googlecode.com/svn/trunk@10 1ddc95d8-fd17-99f2-4e67-9d68e992d05e

--- SmartHMA/src/pl/wasat/smarthma/adapter/CollectionsGroupListAdapter.java
+++ SmartHMA/src/pl/wasat/smarthma/adapter/CollectionsGroupListAdapter.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 import pl.wasat.smarthma.customviews.CollectionsGroupView;
 import pl.wasat.smarthma.model.CollectionsGroup;
-
 import android.content.Context;
 import android.view.ViewGroup;
 
@@ -12,33 +11,35 @@ import com.octo.android.robospice.request.okhttp.simple.OkHttpBitmapRequest;
 import com.octo.android.robospice.spicelist.SpiceListItemView;
 import com.octo.android.robospice.spicelist.okhttp.OkHttpBitmapSpiceManager;
 import com.octo.android.robospice.spicelist.okhttp.OkHttpSpiceArrayAdapter;
-import com.octo.android.robospice.spicelist.simple.SpiceArrayAdapter;
 
-/**
- * An example {@link SpiceArrayAdapter}.
- * @author jva
- * @author stp
- * @author sni
- */
-public class CollectionsGroupListAdapter extends OkHttpSpiceArrayAdapter<CollectionsGroup> {
 
-    // --------------------------------------------------------------------------------------------
-    // CONSTRUCTOR
-    // --------------------------------------------------------------------------------------------
+public class CollectionsGroupListAdapter extends
+		OkHttpSpiceArrayAdapter<CollectionsGroup> {
 
-    public CollectionsGroupListAdapter(Context context, OkHttpBitmapSpiceManager spiceManagerBitmap, CollectionsGroup.List users) {
-        super(context, spiceManagerBitmap, users.getCollectionsGroupList());
-    }
+	// --------------------------------------------------------------------------------------------
+	// CONSTRUCTOR
+	// --------------------------------------------------------------------------------------------
 
-    @Override
-    public OkHttpBitmapRequest createRequest(CollectionsGroup group, int imageIndex, int requestImageWidth, int requestImageHeight) {
-        File tempFile = new File(getContext().getCacheDir(), "THUMB_IMAGE_TEMP_" + group.getGroupName());
-        return new OkHttpBitmapRequest("https://secure.gravatar.com/avatar/" + group.getId(), requestImageWidth, requestImageHeight, tempFile);
-        
-    }
+	public CollectionsGroupListAdapter(Context context,
+			OkHttpBitmapSpiceManager spiceManagerBitmap,
+			CollectionsGroup.List users) {
+		super(context, spiceManagerBitmap, users.getCollectionsGroupList());
+	}
 
-    @Override
-    public SpiceListItemView<CollectionsGroup> createView(Context context, ViewGroup parent) {
-        return new CollectionsGroupView(getContext());
-    }
+	@Override
+	public OkHttpBitmapRequest createRequest(CollectionsGroup group,
+			int imageIndex, int requestImageWidth, int requestImageHeight) {
+		File tempFile = new File(getContext().getCacheDir(),
+				"THUMB_IMAGE_TEMP_" + group.getId());
+		return new OkHttpBitmapRequest("http://89.250.194.14/smarthma/img/sat"
+				+ group.getId() + ".jpeg", requestImageWidth,
+				requestImageHeight, tempFile);
+
+	}
+
+	@Override
+	public SpiceListItemView<CollectionsGroup> createView(Context context,
+			ViewGroup parent) {
+		return new CollectionsGroupView(getContext());
+	}
 }
