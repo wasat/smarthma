56f672ab9d309accc564cd219d89de282a637b00
wasat.pl@gmail.com <wasat.pl@gmail.com@1ddc95d8-fd17-99f2-4e67-9d68e992d05e>
2015-01-16 10:50:10 +0000

    git-svn-id: https://smarthma.googlecode.com/svn/trunk@65 1ddc95d8-fd17-99f2-4e67-9d68e992d05e

---
+++ AndroidStudio/app/src/main/java/pl/wasat/smarthma/model/eo/LinearRing.java
@@ -0,0 +1,141 @@
+package pl.wasat.smarthma.model.eo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import pl.wasat.smarthma.utils.text.SmartHMAStringStyle;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class LinearRing implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String __prefix;
+	private PosString posString;
+	private List<Pos> posList = new ArrayList<Pos>();
+	private final Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+	public String get__prefix() {
+		return __prefix;
+	}
+
+	public void set__prefix(String __prefix) {
+		this.__prefix = __prefix;
+	}
+
+	public LinearRing with__prefix(String __prefix) {
+		this.__prefix = __prefix;
+		return this;
+	}
+
+	public PosString getPosString() {
+		return posString;
+	}
+
+	public void setPosString(PosString posString) {
+		if(posString == null)
+		{
+			posString = new PosString();
+			posString.setPointsString("");
+		}
+		this.posString = posString;
+		
+		if (!posString.getPointsString().isEmpty()
+				&& posString.getPointsString().length() < 200) {
+			this.posList = setPosList(posString.getPointsString());
+		}
+	}
+
+	public LinearRing withPosString(PosString posString) {
+		this.posString = posString;
+		return this;
+	}
+
+	public List<Pos> getPosList() {
+		return posList;
+	}
+
+	public void setPosList(List<Pos> posList) {
+		if (!posList.isEmpty()) {
+			this.posList = posList;
+		}
+		// toPosLatLngList();
+	}
+
+	public List<Pos> setPosList(String pointsString) {
+		String[] coorStr = pointsString.split(" ");
+		List<Pos> latLngPosList = new ArrayList<Pos>();
+
+		//String tempStr = "";
+
+		for (int j = 0; j < coorStr.length - 1; j = j + 2) {
+			LatLng ftPt = new LatLng(Double.valueOf(coorStr[j]),
+					Double.valueOf(coorStr[j + 1]));
+			Pos pos = new Pos();
+			pos.setLatLng(ftPt);
+			latLngPosList.add(pos);
+			//tempStr = tempStr + coorStr[j] + "," + coorStr[j + 1] + ";"
+			//		+ SystemUtils.LINE_SEPARATOR;
+		}
+		//Log.i("FOOT", tempStr);
+		
+
+		return latLngPosList;
+	}
+
+	public LinearRing withPosList(List<Pos> posList) {
+		this.posList = posList;
+		return this;
+	}
+
+	/*
+	 * public List<LatLng> getPosLatLngList() { return posLatLngList; }
+	 * 
+	 * public void setPosLatLngList(List<LatLng> posLatLngList) {
+	 * this.posLatLngList = posLatLngList; }
+	 */
+
+	@Override
+	public String toString() {
+		ToStringStyle style = new SmartHMAStringStyle();
+		ToStringBuilder.setDefaultStyle(style);
+		return ToStringBuilder.reflectionToString(this, style);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return EqualsBuilder.reflectionEquals(this, other);
+	}
+
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+	// private void toPosLatLngList(){
+	// for (int j = 0; j < posList.size(); j++) {
+	// String posStr = posList.get(j).get__text();
+	// LatLng ftPt = new LatLng(Double.valueOf(posStr.split(" ")[0]),
+	// Double.valueOf(posStr.split(" ")[1]));
+	// posLatLngList.add(ftPt);
+	// }
+	// }
+
+}
---
+++ AndroidStudio/app/src/main/java/pl/wasat/smarthma/ui/activities/ProductsBrowserActivity.java
@@ -0,0 +1,183 @@
+package pl.wasat.smarthma.ui.activities;
+
+import java.util.ArrayList;
+import pl.wasat.smarthma.R;
+import pl.wasat.smarthma.helper.Const;
+import pl.wasat.smarthma.model.FedeoRequest;
+import pl.wasat.smarthma.model.eo.Footprint;
+import pl.wasat.smarthma.preferences.SharedPrefs;
+import pl.wasat.smarthma.ui.frags.base.BaseShowProductsListFragment.OnBaseShowProductsListFragmentListener;
+import pl.wasat.smarthma.ui.frags.common.ExtendedMapFragment.OnExtendedMapFragmentListener;
+import pl.wasat.smarthma.ui.frags.common.MetadataFragment.OnMetadataFragmentListener;
+import pl.wasat.smarthma.ui.frags.common.ProductDetailsFragment.OnProductDetailsFragmentListener;
+import pl.wasat.smarthma.ui.frags.common.ExtendedMapFragment;
+import pl.wasat.smarthma.ui.frags.common.ProductsListFragment;
+import pl.wasat.smarthma.ui.frags.dialog.FacebookDialogFragment;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+
+public class ProductsBrowserActivity extends BaseSmartHMActivity implements
+		OnProductDetailsFragmentListener, OnMetadataFragmentListener,
+		OnExtendedMapFragmentListener, OnBaseShowProductsListFragmentListener {
+
+	private ExtendedMapFragment extendedMapFragment;
+	private Footprint mFootprint;
+	private String quicklookUrl;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent intent = getIntent();
+		String parentId = intent.getStringExtra(Const.KEY_INTENT_PARENT_ID);
+
+		SharedPrefs sharedPrefs = new SharedPrefs(getApplicationContext());
+		sharedPrefs.setParentIdPrefs(parentId);
+
+		FedeoRequest fedeoRequest = new FedeoRequest();
+		fedeoRequest.buildFromShared(this);
+
+		ProductsListFragment productsListFragment = ProductsListFragment
+				.newInstance(fedeoRequest);
+		getSupportFragmentManager()
+				.beginTransaction()
+				.replace(R.id.activity_base_list_container,
+						productsListFragment).commit();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		FragmentManager manager = getSupportFragmentManager();
+		FacebookDialogFragment facebookDialogFragment = (FacebookDialogFragment) manager
+				.findFragmentByTag("FacebookDialogFragment");
+		facebookDialogFragment.postOnActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onBackPressed() {
+		FragmentManager fm = getSupportFragmentManager();
+		int bsec = fm.getBackStackEntryCount();
+		if (bsec > 0) {
+			String bstEntry = fm.getBackStackEntryAt(bsec - 1).getName();
+
+			if (bstEntry.equalsIgnoreCase("MetadataFragment")) {
+				fm.popBackStackImmediate("MetadataFragment",
+						FragmentManager.POP_BACK_STACK_INCLUSIVE);
+			} else if (bstEntry.equalsIgnoreCase("ExtendedMapFragment")) {
+				// fm.popBackStackImmediate();
+				super.onBackPressed();
+			} else {
+				bsec = fm.getBackStackEntryCount();
+				if (bsec > 1) {
+					while (bsec > 1) {
+						fm.popBackStackImmediate();
+						bsec = fm.getBackStackEntryCount();
+					}
+				} else {
+					Intent resultIntent = new Intent();
+					resultIntent.putExtra(Const.KEY_INTENT_RETURN_STOP_SEARCH,
+							true);
+					setResult(Activity.RESULT_OK, resultIntent);
+					finish();
+					super.onBackPressed();
+				}
+			}
+		}
+	}
+
+	private void checkMapFragment() {
+		try {
+			if (extendedMapFragment != null) {
+				getSupportFragmentManager().beginTransaction()
+						.remove(extendedMapFragment).commit();
+			}
+
+		} catch (IllegalStateException e) {
+		}
+	}
+
+	@Override
+	public void onMetadataFragmentInteraction() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onProductDetailsFragmentQuicklookShow(String url) {
+		Intent intent = new Intent(Intent.ACTION_VIEW,
+				Uri.parse("content://media/internal/images/media"));
+		intent.setAction(Intent.ACTION_VIEW);
+		intent.setDataAndType(Uri.parse(url), "image/*");
+		startActivity(intent);
+	}
+
+	@Override
+	public void onProductDetailsFragmentExtendedMapShow(String url,
+			Footprint footprint) {
+		quicklookUrl = url;
+		mFootprint = footprint;
+
+		checkMapFragment();
+
+		extendedMapFragment = ExtendedMapFragment.newInstance();
+
+		getSupportFragmentManager()
+				.beginTransaction()
+				.add(R.id.activity_base_details_container, extendedMapFragment,
+						"ExtendedMapFragment")
+				.addToBackStack("ExtendedMapFragment").commit();
+
+	}
+
+	@Override
+	public void onProductDetailsFragmentShareDialogShow(String url) {
+		FacebookDialogFragment dFragment = FacebookDialogFragment.newInstance(url);
+		dFragment.show(getSupportFragmentManager(), "FacebookDialogFragment");
+		
+	}
+	
+	/*
+	 * @Override public void onProductDetailsFragmentMapShow(Footprint
+	 * footprint) { mFootprint = footprint;
+	 * 
+	 * checkMapFragment();
+	 * 
+	 * extendedMapFragment = ExtendedMapFragment.newInstance();
+	 * 
+	 * getSupportFragmentManager() .beginTransaction()
+	 * .add(R.id.activity_base_details_container, extendedMapFragment,
+	 * "ExtendedMapFragment") .addToBackStack("ExtendedMapFragment").commit(); }
+	 */
+
+	@Override
+	public void onMapReady() {
+		if (extendedMapFragment != null) {
+			extendedMapFragment.showQuicklookOnMap(quicklookUrl, mFootprint);
+		}
+	}
+
+	@Override
+	public void onProductDetailsFragmentMetadataLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onBaseShowProductsListFragmentItemSelected(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onBaseShowProductsListFragmentFootprintSend(
+			ArrayList<Footprint> footPrints) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+}
