240125031d4e548f581100f58acc1b394940b915
wasat.pl@gmail.com <wasat.pl@gmail.com@1ddc95d8-fd17-99f2-4e67-9d68e992d05e>
2015-01-20 14:29:16 +0000

    git-svn-id: https://smarthma.googlecode.com/svn/trunk@71 1ddc95d8-fd17-99f2-4e67-9d68e992d05e

---
+++ Amazon/SmartHMA/src/pl/wasat/smarthma/model/feed/Category.java
@@ -0,0 +1,87 @@
+
+package pl.wasat.smarthma.model.feed;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import pl.wasat.smarthma.utils.text.SmartHMAStringStyle;
+
+
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+    private String __prefix;
+    private String _scheme;
+    private String __text;
+    private final Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String get__prefix() {
+        return __prefix;
+    }
+
+    public void set__prefix(String __prefix) {
+        this.__prefix = __prefix;
+    }
+
+    public Category with__prefix(String __prefix) {
+        this.__prefix = __prefix;
+        return this;
+    }
+
+    public String get_scheme() {
+        return _scheme;
+    }
+
+    public void set_scheme(String _scheme) {
+        this._scheme = _scheme;
+    }
+
+    public Category with_scheme(String _scheme) {
+        this._scheme = _scheme;
+        return this;
+    }
+
+    public String get__text() {
+        return __text;
+    }
+
+    public void set__text(String __text) {
+        this.__text = __text;
+    }
+
+    public Category with__text(String __text) {
+        this.__text = __text;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+            	 ToStringStyle style = new SmartHMAStringStyle(); ToStringBuilder.setDefaultStyle(style); return ToStringBuilder.reflectionToString(this, style);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return EqualsBuilder.reflectionEquals(this, other);
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
