e2936ed21d0bfa1416831f4d68aacd930f67e199
wasat.pl@gmail.com <wasat.pl@gmail.com@1ddc95d8-fd17-99f2-4e67-9d68e992d05e>
2015-01-20 14:29:16 +0000

    git-svn-id: https://smarthma.googlecode.com/svn/trunk@71 1ddc95d8-fd17-99f2-4e67-9d68e992d05e

---
+++ Amazon/SmartHMA/src/pl/wasat/smarthma/interfaces/OnCollectionsListSelectionListener.java
@@ -0,0 +1,9 @@
+package pl.wasat.smarthma.interfaces;
+
+public interface OnCollectionsListSelectionListener {
+	
+	public void onCollectionSelected(Integer chosenCollectionId);
+
+	boolean isTwoPaneMode();
+
+}
---
+++ Amazon/SmartHMA/src/pl/wasat/smarthma/model/eo/ResultTime.java
@@ -0,0 +1,73 @@
+package pl.wasat.smarthma.model.eo;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import pl.wasat.smarthma.utils.text.SmartHMAStringStyle;
+
+public class ResultTime implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String __prefix;
+	private TimeInstant timeInstant;
+	private final Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+	public String get__prefix() {
+		return __prefix;
+	}
+
+	public void set__prefix(String __prefix) {
+		this.__prefix = __prefix;
+	}
+
+	public ResultTime with__prefix(String __prefix) {
+		this.__prefix = __prefix;
+		return this;
+	}
+
+	public TimeInstant getTimeInstant() {
+		return timeInstant;
+	}
+
+	public void setTimeInstant(TimeInstant timeInstant) {
+		this.timeInstant = timeInstant;
+	}
+
+	public ResultTime withTimeInstant(TimeInstant timeInstant) {
+		this.timeInstant = timeInstant;
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		ToStringStyle style = new SmartHMAStringStyle();
+		ToStringBuilder.setDefaultStyle(style);
+		return ToStringBuilder.reflectionToString(this, style);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return EqualsBuilder.reflectionEquals(this, other);
+	}
+
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+}
